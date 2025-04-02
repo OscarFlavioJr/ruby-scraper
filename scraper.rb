00f5a37c5006ad04828620c78283ce6892ee11b0
Oscar Flávio Jr <oscarflaviojunior@gmail.com>
2025-04-02 14:07:59 -0300

    adição de selenium

--- scraper.rb
+++ scraper.rb
@@ -1,13 +1,31 @@
 require "httparty"
 require "nokogiri"
+require "selenium-webdriver"
 
-response = HTTParty.get("https://www.vagas.com.br/vagas-de-Fleury", 
-    headers: {
-        "User-agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-    }
-)
+options = Selenium::WebDriver::Chrome::Options.new
+options.add_argument("--headless") 
+driver = Selenium::WebDriver.for :chrome, options: options
+driver.navigate.to "https://www.vagas.com.br/vagas-de-Fleury"
 
-document = Nokogiri::HTML(response.body)
+sleep 3  
+
+while true
+  begin
+    button = driver.find_element(id: "maisVagas")
+    driver.execute_script("arguments[0].scrollIntoView();", button)
+    button.click
+    sleep 2  
+  rescue Selenium::WebDriver::Error::NoSuchElementError
+    puts "Todas as vagas foram carregadas!"
+    break
+  rescue Selenium::WebDriver::Error::StaleElementReferenceError
+    puts "Elemento atualizado, tentando novamente..."
+    next
+  end
+end    
+
+html = driver.page_source
+document = Nokogiri::HTML(html)
 
 num_vagas = document.at_css("div.faixa.clearfix h1")&.text&.strip
 puts "#{num_vagas} disponíveis, sendo elas:"
@@ -15,8 +33,11 @@ puts "#{num_vagas} disponíveis, sendo elas:"
 vagas = document.css("div.informacoes-header")
 
 vagas.each do |vaga|
-    cargo = vaga.at_css("h2.cargo")&.text&.strip
-    link = vaga.at_css("h2.cargo a.link-detalhes-vaga")&.[]("href")
-puts "Cargo: #{cargo}"
-puts "Link: https:/#{link}"
+  cargo = vaga.at_css("h2.cargo")&.text&.strip
+  link = vaga.at_css("h2.cargo a.link-detalhes-vaga")&.[]("href")
+
+  puts "Cargo: #{cargo}"
+  puts "Link: https://www.vagas.com.br#{link}"
 end
+
+driver.quit
