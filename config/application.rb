3f992eb2630c386de3181ee965796a444b71d38d
Oscar Flávio Jr <oscarflaviojunior@gmail.com>
2025-04-03 14:41:30 -0300

    É pago, mas não vou pagar pra rodar o serviço várias vezes seguidas. O código funciona!

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,18 @@
 require 'sidekiq'
 require 'sidekiq-cron'
 
-redis_url = ENV['REDIS_URL']
+redis_url = ENV['REDIS_URL'] || raise("Environment variable REDIS_URL is not set")
+redis_namespace = 'sidekiq'
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: redis_url }
+  config.redis = { url: redis_url, namespace: redis_namespace }
+
+  schedule_file = File.expand_path("../config/sidekiq.yml", __FILE__)
+  schedule_file_exists = File.exist?(schedule_file)
 
   config.on(:startup) do
-    schedule_file = File.expand_path("../config/sidekiq.yml", __FILE__)
-
-    if File.exist?(schedule_file)
-      Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+    if schedule_file_exists
+      Sidekiq::Cron::Job.load_from_hash YAML.safe_load(File.read(schedule_file), permitted_classes: [Symbol])
     end
   end
 end
